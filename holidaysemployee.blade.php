f6567821e5756e839c73af25f2c329fe2c25397c
Hell-loW <102574019+Hell-loW@users.noreply.github.com>
2022-05-11 21:53:35 +0800

    Add files via upload
    new

---
+++ holidaysemployee.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.master')
+@section('content')
+
+    <!-- Sidebar -->
+    <div class="sidebar" id="sidebar">
+        <div class="sidebar-inner slimscroll">
+            <div id="sidebar-menu" class="sidebar-menu">
+                <ul>
+                    <li class="menu-title">
+                        <span>Main</span>
+                    </li>
+                    <li class="submenu">
+                        <a href="#" class="noti-dot">
+                            <i class="la la-dashboard"></i>
+                            <span> Dashboard</span>
+                            <span class="menu-arrow"></span>
+                        </a>
+                        <ul style="display: none;">
+                            <li><a class="active" href="{{ route('em/dashboard') }}">Employee Dashboard</a></li>
+                        </ul>
+                    </li>
+                    @if (Auth::user()->role_name=='Admin')
+                        <li class="menu-title"> <span>Authentication</span> </li>
+                        <li class="submenu">
+                            <a href="#">
+                                <i class="la la-user-secret"></i>
+                                <span> User Controller</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul style="display: none;">
+                                <li><a href="{{ route('userManagement') }}">All User</a></li>
+                                <li><a href="{{ route('activity/log') }}">Activity Log</a></li>
+                                <li><a href="{{ route('activity/login/logout') }}">Activity User</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                    <li class="menu-title"> <span>Employees</span> </li>
+                    <li class="submenu">
+                        <a href="#">
+                            <i class="la la-user"></i>
+                            <span> Employees</span>
+                            <span class="menu-arrow"></span>
+                        </a>
+                        <ul style="display: none;">
+                            <li><a href="{{ route('attendance/employee/page') }}">Attendance</a></li>
+                            <li><a href="{{route('form/leavesemployee/new')}}">Leaves</a></li>
+                            <li><a href="{{route('form/holidaysemployee/new')}}">Holidays</a></li>
+                            <li><a href="{{ route('form/leavesettings/page') }}">Leave Settings</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-title"> <span>Pages</span> </li>
+                    <li class="submenu"> <a href="#"><i class="la la-user"></i>
+                        <span> Profile </span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="{{ url('form/employeeprofile') }}"> Employee Profile </a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /Sidebar -->
+
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <!-- Page Content -->
+        <div class="content container-fluid">
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="page-title">Holidays <span id="year"></span></h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Holidays</li>
+                        </ul>
+                    </div>
+                    <div class="col-auto float-right ml-auto">
+                        <a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_holiday"><i class="fa fa-plus"></i> Add Holiday</a>
+                    </div>
+                </div>
+            </div>
+			<!-- /Page Header -->
+        <!-- Add Holiday Modal -->
+        <div class="modal custom-modal fade" id="add_holiday" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add Holiday</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('form/holidays/save') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label>Holiday Name <span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="nameHoliday" name="nameHoliday">
+                            </div>
+                            <div class="form-group">
+                                <label>Holiday Date <span class="text-danger">*</span></label>
+                                <div class="cal-icon">
+                                    <input class="form-control datetimepicker" type="text" id="holidayDate" name="holidayDate">
+                                </div>
+                            </div>
+                            <div class="submit-section">
+                                <button type="submit" class="btn btn-primary submit-btn">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Add Holiday Modal -->
+
+        <!-- Edit Holiday Modal -->
+        <div class="modal custom-modal fade" id="edit_holiday" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Edit Holiday</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('form/holidays/update') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="id" id="e_id" value="">
+                            <div class="form-group">
+                                <label>Holiday Name <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" id="holidayName_edit" name="holidayName" value="">
+                            </div>
+                            <div class="form-group">
+                                <label>Holiday Date <span class="text-danger">*</span></label>
+                                <div class="cal-icon">
+                                    <input type="text" class="form-control datetimepicker" id="holidayDate_edit" name="holidayDate" value="">
+                                </div>
+                            </div>
+                            <div class="submit-section">
+                                <button type="submit" class="btn btn-primary submit-btn">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Edit Holiday Modal -->
+
+        <!-- Delete Holiday Modal -->
+        <div class="modal custom-modal fade" id="delete_holiday" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="form-header">
+                            <h3>Delete Holiday</h3>
+                            <p>Are you sure want to delete?</p>
+                        </div>
+                        <div class="modal-btn delete-action">
+                            <div class="row">
+                                <div class="col-6">
+                                    <a href="javascript:void(0);" class="btn btn-primary continue-btn">Delete</a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="javascript:void(0);" data-dismiss="modal" class="btn btn-primary cancel-btn">Cancel</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Delete Holiday Modal -->
+       
+    </div>
+    <!-- /Page Wrapper -->
+    @section('script')
+    <script>
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+    </script>
+    {{-- update js --}}
+    <script>
+        $(document).on('click','.userUpdate',function()
+        {
+            var _this = $(this).parents('tr');
+            $('#e_id').val(_this.find('.id').text());
+            $('#holidayName_edit').val(_this.find('.holidayName').text());
+            $('#holidayDate_edit').val(_this.find('.holidayDate').text());  
+        });
+    </script>
+    @endsection
+
+@endsection
